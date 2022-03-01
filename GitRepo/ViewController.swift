f26c498ab87bdc6a8a1e0ee161f9dd5ef4e309f2
tunetab <s.panov@g.nsu.ru>
2022-03-01 16:53:54 +0700

    Initial Commit

---
+++ GitRepo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitRepo
+//
+//  Created by Стажер on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
