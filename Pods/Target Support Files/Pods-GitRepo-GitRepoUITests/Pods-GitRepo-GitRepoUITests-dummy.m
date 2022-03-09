931b45e92981158aebc6dad4c6d32efdbe8b22a7
tunetab <s.panov@g.nsu.ru>
2022-03-09 17:48:20 +0700

    bug fixes

---
+++ Pods/Target Support Files/Pods-GitRepo-GitRepoUITests/Pods-GitRepo-GitRepoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitRepo_GitRepoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GitRepo_GitRepoUITests
+@end
---
+++ Pods/Target Support Files/Pods-GitRepoTests/Pods-GitRepoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitRepoTests : NSObject
+@end
+@implementation PodsDummy_Pods_GitRepoTests
+@end
